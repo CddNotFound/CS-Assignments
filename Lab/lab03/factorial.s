b40114c9c4c59c877f297c7936cb727c8c4e339f
CddNotFound <kkcdd@outlook.com>
2025-10-09 10:30:16 +0800

    lab03

--- Lab/lab03/factorial.s
+++ Lab/lab03/factorial.s
@@ -22,3 +22,29 @@ main:
 
 factorial:
     # YOUR CODE HERE
+
+    beq a0, zero, return1
+
+    addi sp, sp, -8
+    sw s0, 0(sp)   # s0: n
+    sw ra, 4(sp)
+    mv s0, a0
+
+    li t0, 1       # t0: i
+    li a0, 1
+factorial_loop_begin:
+    blt s0, t0, factorial_loop_end
+    mul a0, a0, t0
+    addi t0, t0, 1
+    j factorial_loop_begin
+
+factorial_loop_end:
+
+    lw s0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    ret
+
+return1:
+    li a0, 1
+    ret
