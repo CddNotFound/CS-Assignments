755d090e7a8ec6c2d319a46a10539bbc58cdb553
CddNotFound <kkcdd@outlook.com>
2025-10-09 09:04:41 +0800

    proj2

--- Project/proj2/src/read_matrix.s
+++ Project/proj2/src/read_matrix.s
@@ -27,16 +27,133 @@
 read_matrix:
 
     # Prologue
-	
 
+    addi sp, sp, -24
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw ra, 20(sp)
+    mv s0, a0
+    mv s1, a1  # s1: pointer to n
+    mv s2, a2  # s2: pointer to m
+               # s3: n
+               # s4: m
 
+    # fopen
+    mv a1, s0
+    li a2, 0   # read
+    jal fopen
+    mv t0, a0  # t0: file descriptor
+    li t1, -1  # t1: -1
+    beq t0, t1, fopen_err
 
+    # read n, m
+    addi sp, sp, -4
+    sw t0, 0(sp)
 
+    mv a1, t0
+    mv a2, s1
+    li a3, 4
+    jal fread
+    li t1, 4
 
+    lw t0, 0(sp)
+    addi sp, sp, 4
+    bne a0, t1, fread_err
 
+    addi sp, sp, -4
+    sw t0, 0(sp)
 
+    mv a1, t0
+    mv a2, s2
+    li a3, 4
+    jal fread
+    li t1, 4
 
+    lw t0, 0(sp)
+    addi sp, sp, 4
+    bne a0, t1, fread_err
+
+    lw s3, 0(s1)
+    lw s4, 0(s2)
+
+    # malloc
+    addi sp, sp, -4
+    sw t0, 0(sp)
+
+    mul a0, s3, s4
+    li t6, 4   # t6: 4
+    mul a0, a0, t6
+    jal malloc
+    mv t4, a0  # t4: pointer to a[]
+
+    lw t0, 0(sp)
+    addi sp, sp, 4
+    beq t4, zero, malloc_fail
+
+    # fread
+    addi sp, sp, -8
+    sw t0, 0(sp)
+    sw t4, 4(sp)
+
+    mv a1, t0
+    mv a2, t4
+    li a3, 4
+    mul a3, a3, s3
+    mul a3, a3, s4  # n * m * 4 bytes
+
+    addi sp, sp, -4
+    sw a3, 0(sp)
+    jal fread
+    lw a3, 0(sp)
+    addi sp, sp, 4
+
+    lw t0, 0(sp)
+    lw t4, 4(sp)
+    addi sp, sp, 8
+    bne a0, a3, fread_err
+
+    # fclose
+    addi sp, sp, -8
+    sw t0, 0(sp)
+    sw t4, 4(sp)
+
+    mv a1, t0
+    jal fclose
+
+    lw t0, 0(sp)
+    lw t4, 4(sp)
+    addi sp, sp, 8
+    li t1, -1
+    beq a0, t1, fclose_err
+
+    mv a0, t4
     # Epilogue
 
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw ra, 20(sp)
+    addi sp, sp, 24
 
     ret
+
+malloc_fail:
+    li a1, 88
+    jal exit2
+
+fopen_err:
+    li a1, 90
+    jal exit2
+
+fread_err:
+    li a1, 91
+    jal exit2
+
+fclose_err:
+    li a1, 92
+    jal exit2
--- Project/proj2/src/write_matrix.s
+++ Project/proj2/src/write_matrix.s
@@ -26,15 +26,85 @@ write_matrix:
 
     # Prologue
 
+    addi sp, sp, -24
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw ra, 20(sp)
+    mv s0, a0  # s0: filename
+    mv s1, a1  # s1: *a
+    mv s2, a2  # s2: n
+    mv s3, a3  # s3: m
 
+    # fopen
+    mv a1, s0
+    li a2, 1   # write
+    jal fopen
+    mv s4, a0  # s4: file descriptor
+    li t1, -1  # t1: -1
+    beq s4, t1, fopen_err
 
+    # write n, m
+    addi sp, sp, -4
+    sw s2, 0(sp)
+    mv a1, s4
+    mv a2, sp
+    li a3, 1
+    li a4, 4
+    jal fwrite
+    addi sp, sp, 4
+    li t1, 1
+    blt a0, t1, fwrite_err
+    
+    addi sp, sp, -4
+    sw s3, 0(sp)
+    mv a1, s4
+    mv a2, sp
+    li a3, 1
+    li a4, 4
+    jal fwrite
+    addi sp, sp, 4
+    li t1, 1
+    blt a0, t1, fwrite_err
 
+    # write a[][]
+    mv a1, s4
+    mv a2, s1
+    mul a3, s2, s3
+    li a4, 4
+    jal fwrite
+    mul t1, s2, s3
+    blt a0, t1, fwrite_err
 
-
-
-
+    # fclose
+    mv a1, s4
+    jal fclose
+    li t1, -1
+    beq a0, t1, fclose_err
 
     # Epilogue
 
+    mv a0, zero
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw ra, 20(sp)
+    addi sp, sp, 24
 
     ret
+
+fopen_err:
+    li a1, 93
+    jal exit2
+
+fwrite_err:
+    li a1, 94
+    jal exit2
+
+fclose_err:
+    li a1, 95
+    jal exit2
