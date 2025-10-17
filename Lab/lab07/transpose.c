8f987148e56d6be81658b00e9a6d82ea6de4c6e9
CddNotFound <kkcdd@outlook.com>
2025-10-17 11:45:36 +0800

    finish lab07

--- Lab/lab07/transpose.c
+++ Lab/lab07/transpose.c
@@ -10,7 +10,14 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 }
 
 /* Implement cache blocking below. You should NOT assume that n is a
- * multiple of the block size. */
+* multiple of the block size. */
+#define min(X, Y) ((X) < (Y) ? (X) : (Y))
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    for (int i = 0; i <= n / blocksize; i++)
+        for (int j = 0; j <= n / blocksize; j++) {
+            for (int x = i * blocksize; x < (i + 1) * blocksize && x < n; x++)
+                for (int y = j * blocksize; y < (j + 1) * blocksize && y < n; y++)
+                    dst[y + x * n] = src[x + y * n];
+        }
 }
