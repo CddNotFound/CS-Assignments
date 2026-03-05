80d316b651dd66c200da6a7dbefa760880f99f1a
CddNotFound <kkcdd@outlook.com>
2026-03-05 16:08:30 +0800

    移动位置 + 6.092 - lab02

---
+++ NJU - Compiler/lab02/FooCorporation.java
@@ -0,0 +1,14 @@
+
+class Square2 {
+    public static void printSquare(int x) {
+        System.out.println(x*x);
+    }
+    public static void times(int a, int b) {
+        System.out.println(a*b);
+    }
+    public static void main(String[] arguments) {
+        printSquare(5);
+        times(2, 3);
+        times(5, 3);
+    }
+}
