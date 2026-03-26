fb0e7ca835ec31c5697bbeb12c84ec8a34f33e08
tracer-compiler2022 <compilers@software.nju.edu.cb>
2026-03-26 22:05:56 +0800

    > make xxx cdd Linux cddnotfound 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:05:56 up  9:54,  3 users,  load average: 0.05, 0.05, 0.01 d291d1cec8bd7ca2892afb79a22896afab8eaba

--- NJU - Compiler/lab1/src/Main.java
+++ NJU - Compiler/lab1/src/Main.java
@@ -3,30 +3,41 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import .antlr.SysYLexer;
+
 public class Main
 {    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length < 1) {
-            return ;
+            System.err.println("input path is required");
         }
+        String source = args[0];
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+    }
+    
+    // public static void main(String[] args) {
+    //     if (args.length < 1) {
+    //         return ;
+    //     }
 
-        String file = args[0];
-        try (FileReader fr = new FileReader(file)) {
-            BufferedReader br = new BufferedReader(fr);
+    //     String file = args[0];
+    //     try (FileReader fr = new FileReader(file)) {
+    //         BufferedReader br = new BufferedReader(fr);
 
 
             
-            String str = br.readLine();
-            if (str == null) {
-                return ;
-            }
-            System.out.print(str);
-            while ((str = br.readLine()) != null) {
-                System.out.println();
-                System.out.print(str);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+    //         String str = br.readLine();
+    //         if (str == null) {
+    //             return ;
+    //         }
+    //         System.out.print(str);
+    //         while ((str = br.readLine()) != null) {
+    //             System.out.println();
+    //             System.out.print(str);
+    //         }
+    //     } catch (IOException e) {
+    //         System.out.println(e.getMessage());
+    //     }
+    // }
 }
