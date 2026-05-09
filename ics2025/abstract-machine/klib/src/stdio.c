d427808891d4ee548ccb68b4fdb69a71cdba37f7
CddNotFound <kkcdd@outlook.com>
2026-05-09 15:05:27 +0800

    pa2: pass test string.c & hello-str.c

--- ics2025/abstract-machine/klib/src/stdio.c
+++ ics2025/abstract-machine/klib/src/stdio.c
@@ -13,8 +13,57 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+static char * putStr(char *out, const char* s) {
+  while (*s) {
+    *out++ = *s++;
+  }
+  return out;
+}
+static char * putChar(char *out, const char s) {
+  *out++ = s;
+  return out;
+}
+static char * putInt(char *out, int d) {
+  char buf[16];
+  int cnt = 0;
+  while (d) {
+    buf[++cnt] = (char)(d % 10 + 48);
+    d /= 10;
+  }
+
+  while (cnt) {
+    *out++ = buf[cnt];
+    cnt -= 1;
+  }
+
+  return out;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+
+  va_start(ap, fmt);
+  while (*fmt) {
+    if (*fmt != '%') {
+      *out++ = *fmt++;
+      continue;
+    }
+
+    fmt++;
+
+    switch (*fmt++) {
+      case 's': out = putStr(out, va_arg(ap, char *)); break;
+      case 'd': out = putInt(out, va_arg(ap, int)); break;
+      case 'c': out = putChar(out, (char)va_arg(ap, int)); break;
+      case '%': *out++ = '%'; break;
+      default: break;
+    }
+  }
+  va_end(ap);
+
+  *out = '\0';
+
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
