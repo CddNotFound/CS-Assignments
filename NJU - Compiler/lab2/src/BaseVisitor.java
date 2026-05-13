d6779da55acf8c78203f546edf55830f345a759e
CddNotFound <kkcdd@outlook.com>
2026-05-13 18:46:32 +0800

    lab2: passed. (增加 break | continue、颜色采用栈实现、单ret无空格 | 单元运算符仅让自己无前后空格、删除行末空格）

--- NJU - Compiler/lab2/src/BaseVisitor.java
+++ NJU - Compiler/lab2/src/BaseVisitor.java
@@ -10,17 +10,21 @@ import org.antlr.v4.runtime.Vocabulary;
 import org.antlr.v4.runtime.misc.*;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
     HashMap<Integer, TokenStyle> mp;
     // int parenCnt, bracktCnt, braceCnt;
     int braceCnt;
     int level;
-    int color;
-    boolean underline, blockInBlock, declare, unaryOp;
+    boolean blockInBlock, unaryOp;
     boolean firstRow;
+    ArrayList<Integer> color;
+    ArrayList<Boolean> declare, underline;
+    int colorCnt, delcareCnt, underlineCnt;
     public static final int[] colorList = new int[6];
     int a, b;
+    boolean singleRet;
 
     static {
         for (int i = 0; i < 6; i++) {
@@ -32,14 +36,19 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
         mp = new HashMap<Integer, TokenStyle>();
         level = 0;
         braceCnt = 0;
-        color = 0;
-        underline = false;
         unaryOp = false;
         blockInBlock = false;
-        declare = false;
         firstRow = true;
         a = 0;
         b = 0;
+        colorCnt = 1; delcareCnt = 1; underlineCnt = 1;
+        color = new ArrayList<Integer>(Collections.nCopies(1000, 0));
+        declare = new ArrayList<Boolean>(Collections.nCopies(1000, false));
+        underline = new ArrayList<Boolean>(Collections.nCopies(1000, false));
+        color.set(1, 0);
+        declare.set(1, false);
+        underline.set(1, false);
+        singleRet = false;
     }
 
     private void printTab() {
@@ -53,7 +62,7 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
 
     private void printToken(String text, int color, boolean underline) {
         firstRow = false;
-        if (text == "<EOF>") {
+        if (text.equals("<EOF>")) {
             return ;
         }
 
@@ -74,7 +83,7 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
     }
 
     private void printFollowingSpace(int type) {
-        if (((type >= 1 && type <= 3) || type == 7 || type == 8 || type == 4 || type == 6)
+        if (((type >= 1 && type <= 3) || type == 7 || type == 4 || (type == 6 && !singleRet))
          || (type >= 12 && type <= 21 || (type >= 23 && type <= 26))
          || ((type == 10 || type == 11) && !unaryOp)
          || type == 27) {
@@ -89,6 +98,10 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
         b = interval.b;
         return visitChildren(ctx); 
     }
+    // @Override public T visitLVal(SysYParser.LValContext ctx) {
+    //     visitChildren(ctx);
+    //     return null;
+    // }
 
     @Override public T visitTerminal(TerminalNode node) {
         Token token = node.getSymbol();
@@ -96,34 +109,35 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
         String text = token.getText();
         int idx = token.getTokenIndex();
 
-        int sColor = color;
-        boolean sUnderline = underline;
+        if ((type >= 1 && type <= 29) || (type >= 35 && type <= 40)) {
+            ++colorCnt;
+        }
 
         if (type >= 1 && type <= 9) { // key word : Bright Cyan
-            color = 96;
+            color.set(colorCnt, 96);
         } else if (type >= 10 && type <= 28) { // operator : Bright Red
-            color = 91;
+            color.set(colorCnt, 91);
         } else if (type == 29) { // const int : Magenta
-            color = 35;
+            color.set(colorCnt, 35);
         } else if (type == 37 || type == 35 || type == 39) {
-            color = colorList[braceCnt % 6];
+            color.set(colorCnt, colorList[braceCnt % 6]);
             ++braceCnt;
         } else if (type == 38 || type == 36 || type == 40) {
             --braceCnt;
-            color = colorList[braceCnt % 6];
+            color.set(colorCnt, colorList[braceCnt % 6]);
         }
 
         printPreviousSpace(type);
 
-        if (declare) {
+        if (declare.get(delcareCnt)) {
             // System.err.print("delcare: ");
-            printToken(text, color, underline);
+            printToken(text, color.get(colorCnt), underline.get(underlineCnt));
         } else if (type == 37) {
             // System.err.print("L_BRACE: ");
             if (blockInBlock) {
                 println();
                 printTab();
-                printToken(text, color, underline);
+                printToken(text, color.get(colorCnt), underline.get(underlineCnt));
                 level += 1;
             } else {
                 ParseTree parent = node.getParent().getParent().getParent();
@@ -135,7 +149,7 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
                 } else {
                     System.out.print(" ");
                 }
-                printToken(text, color, underline);
+                printToken(text, color.get(colorCnt), underline.get(underlineCnt));
                 level += 1;
             }
         } else if (type == 38) {
@@ -143,7 +157,7 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
             println();
             level -= 1;
             printTab();
-            printToken(text, color, underline);
+            printToken(text, color.get(colorCnt), underline.get(underlineCnt));
         } else if (type == 8) {
             SysYParser.IfElseContext ftr = (SysYParser.IfElseContext) node.getParent();
             if (!(ftr.stat(0) instanceof SysYParser.CodeBlockContext)) {
@@ -151,21 +165,53 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
             }
             println();
             printTab();
-            printToken(text, color, underline);
+            printToken(text, color.get(colorCnt), underline.get(underlineCnt));
         } else {
-            printToken(text, color, underline);
+            printToken(text, color.get(colorCnt), underline.get(underlineCnt));
         }
 
         printFollowingSpace(type);
 
-        color = sColor;
-        underline = sUnderline;
+        if ((type >= 1 && type <= 29) || (type >= 35 && type <= 40)) {
+            --colorCnt;
+        }
 
         return (T)this.defaultResult();
     }
 
-    @Override public T visitBlock(SysYParser.BlockContext ctx) {
+    @Override public T visitBreak(SysYParser.BreakContext ctx) {
+        stmtTab(ctx.getParent());
+        T tmp = visitChildren(ctx);
+        exitStmt(ctx.getParent());
+        return null;
+    }
+    @Override public T visitEmpty(SysYParser.EmptyContext ctx) {
+        stmtTab(ctx.getParent());
         
+        ++colorCnt;
+        ++underlineCnt;
+
+        color.set(colorCnt, 0);
+        underline.set(underlineCnt, false);
+
+        T tmp = visitChildren(ctx);
+
+        --colorCnt;
+        --underlineCnt;
+
+        exitStmt(ctx.getParent());
+
+        return tmp;
+    }
+    @Override public T visitContinue(SysYParser.ContinueContext ctx) {
+        stmtTab(ctx.getParent());
+        T tmp = visitChildren(ctx);
+        exitStmt(ctx.getParent());
+        return null;
+    }
+
+
+    @Override public T visitBlock(SysYParser.BlockContext ctx) {
         ParseTree parent = ctx.getParent().getParent();
         if (parent instanceof SysYParser.IfElseContext
          || parent instanceof SysYParser.WhileLoopContext
@@ -186,62 +232,71 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
             println();
         }
         printTab();
-        int sColor = color;
-        boolean sUnderline = underline;
 
-        color = 93;
+        visit(ctx.funcDecl().allType());
 
-        T tmp = visitChildren(ctx);
+        ++colorCnt;
 
-        color = sColor;
-        underline = sUnderline;
+        color.set(colorCnt, 93);
+        visit(ctx.funcDecl().IDENT());
+        --colorCnt;
+
+        visit(ctx.funcDecl().L_PAREN());
+        if (ctx.funcDecl().parameters() != null) {
+            visit(ctx.funcDecl().parameters());
+        }
+        visit(ctx.funcDecl().R_PAREN());
+        visit(ctx.funcDecl().block());
         
-        return tmp;
+        return null;
     }
 
     @Override public T visitParameters(SysYParser.ParametersContext ctx) {
-        int sColor = color;
-        boolean sUnderline = underline;
+        // ++colorCnt;
 
-        color = 0;
+        // color.set(colorCnt, 0);
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
+        // --colorCnt;
         
         return tmp;
     }
     
     @Override public T visitFunctionCall(SysYParser.FunctionCallContext ctx) {
-        int sColor = color;
-        boolean sUnderline = underline;
+        ++colorCnt;
 
-        color = 93;
-        underline = false;
+        color.set(colorCnt, 93);
 
-        T tmp = visitChildren(ctx);
+        visit(ctx.IDENT());
 
-        color = sColor;
-        underline = sUnderline;
+        --colorCnt;
 
-        return tmp;
+        visit(ctx.L_PAREN());
+        if (ctx.funcRParams() != null) {
+            visit(ctx.funcRParams());
+        }
+        visit(ctx.R_PAREN());
+
+
+        return null;
     }
 
 	@Override public T visitVariableDecl(SysYParser.VariableDeclContext ctx) {    
         stmtTab(ctx.getParent());
-        int sColor = color;
-        boolean sUnderline = underline, sDeclare = declare;
+        ++colorCnt;
+        ++underlineCnt;
+        ++delcareCnt;
 
-        color = 95;
-        underline = true;
-        declare = true;
+        color.set(colorCnt, 95);
+        underline.set(underlineCnt, true);
+        declare.set(delcareCnt, true);
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
-        declare = sDeclare;
+        --colorCnt;
+        --underlineCnt;
+        --delcareCnt;
 
         exitStmt(ctx.getParent());
         return tmp;
@@ -249,16 +304,16 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
 
     @Override public T visitWhileLoop(SysYParser.WhileLoopContext ctx) {
         stmtTab(ctx.getParent());
-        int sColor = color;
-        boolean sUnderline = underline;
+        ++colorCnt;
+        ++underlineCnt;
 
-        color = 97;
-        underline = false;
+        color.set(colorCnt, 97);
+        underline.set(underlineCnt, false);
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
+        --colorCnt;
+        --underlineCnt;
 
         exitStmt(ctx.getParent());
         return tmp;
@@ -280,16 +335,34 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
             println();
             printTab();
         }
-        int sColor = color;
-        boolean sUnderline = underline;
+        
+        ++colorCnt;
+        ++underlineCnt;
 
-        color = 97;
-        underline = false;
+        color.set(colorCnt, 97);
+        underline.set(underlineCnt, false);
 
-        T tmp = visitChildren(ctx);
+        visit(ctx.IF());
+        visit(ctx.L_PAREN());
+        visit(ctx.cond());
+        visit(ctx.R_PAREN());
+        visit(ctx.stat(0));
 
-        color = sColor;
-        underline = sUnderline;
+        if (ctx.ELSE() != null) {
+            // println();
+            // printTab();
+            visit(ctx.ELSE());
+
+            if (ctx.stat(1) instanceof SysYParser.IfElseContext
+            || ctx.stat(1) instanceof SysYParser.CodeBlockContext) {
+                System.out.print(" ");
+            }
+
+            visit(ctx.stat(1));
+        }
+
+        --colorCnt;
+        --underlineCnt;
 
         if (parent instanceof SysYParser.IfElseContext) {
             SysYParser.IfElseContext ftr = (SysYParser.IfElseContext) parent;
@@ -301,21 +374,25 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
         }
         // println();
         
-        return tmp;
+        return null;
     }
 
 	@Override public T visitReturn(SysYParser.ReturnContext ctx) {
         stmtTab(ctx.getParent());
-        int sColor = color;
-        boolean sUnderline = underline;
+        ++colorCnt;
+        ++underlineCnt;
 
-        color = 97;
-        underline = false;
+        color.set(colorCnt, 97);
+        underline.set(underlineCnt, false);
+        
+        if (ctx.exp() == null) { singleRet = true;}
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
+        singleRet = false;
+
+        --colorCnt;
+        --underlineCnt;
         
         exitStmt(ctx.getParent());
         return tmp;
@@ -323,16 +400,16 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
     
 	@Override public T visitExpd(SysYParser.ExpdContext ctx) {
         stmtTab(ctx.getParent());
-        int sColor = color;
-        boolean sUnderline = underline;
+        ++colorCnt;
+        ++underlineCnt;
 
-        color = 97;
-        underline = false;
+        color.set(colorCnt, 97);
+        underline.set(underlineCnt, false);
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
+        --colorCnt;
+        --underlineCnt;
         
         exitStmt(ctx.getParent());
         return tmp;
@@ -340,16 +417,16 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
 
     @Override public T visitVarAssign(SysYParser.VarAssignContext ctx) {
         stmtTab(ctx.getParent());
-        int sColor = color;
-        boolean sUnderline = underline;
+        ++colorCnt;
+        ++underlineCnt;
 
-        color = 97;
-        underline = false;
+        color.set(colorCnt, 97);
+        underline.set(underlineCnt, false);
 
         T tmp = visitChildren(ctx);
 
-        color = sColor;
-        underline = sUnderline;
+        --colorCnt;
+        --underlineCnt;
         
         exitStmt(ctx.getParent());
         return tmp;
@@ -360,11 +437,13 @@ public class BaseVisitor<T> extends SysYParserBaseVisitor<T>{
 
         unaryOp = true;
 
-        T tmp = visitChildren(ctx);
+        visit(ctx.unaryOp());
 
         unaryOp = sUnaryOp;
 
-        return tmp;
+        visit(ctx.exp());
+
+        return null;
     }
 
     public void stmtTab(ParseTree parent) {
