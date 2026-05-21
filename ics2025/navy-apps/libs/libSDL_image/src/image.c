a50f9335b83088bca0f3b5b270a5662ed0350318
CddNotFound <kkcdd@outlook.com>
2026-05-21 15:35:31 +0800

    pa3: complete Flappy Bird

--- ics2025/navy-apps/libs/libSDL_image/src/image.c
+++ ics2025/navy-apps/libs/libSDL_image/src/image.c
@@ -5,6 +5,9 @@
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
 
+#include <fcntl.h>
+#include <unistd.h>
+
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
   assert(freesrc == 0);
@@ -12,7 +15,20 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  int fd = open(filename, 1);
+  assert(fd != -1);
+  int fileStart = lseek(fd, 0, SEEK_SET);
+  int fileEnd = lseek(fd, 0, SEEK_END);
+  int fileSize = fileEnd - fileStart;
+
+  char *image = malloc(sizeof(char) * fileSize);
+  assert(image);
+
+  read(fd, image, fileSize);
+  SDL_Surface *s = STBIMG_LoadFromMemory(image, fileSize);
+
+  close(fd);
+  return s;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
